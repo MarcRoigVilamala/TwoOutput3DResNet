ef0dac16b8c3643e3a6d3c73ea10c22176168404
Marc Roig Vilamala <marc.roig.vilamala@gmail.com>
2019-07-25 13:12:18 +0100

    Improved how datasets are created and added UCFCRIME

--- dataset.py
+++ dataset.py
@@ -2,145 +2,86 @@ from datasets.kinetics import Kinetics
 from datasets.activitynet import ActivityNet
 from datasets.ucf101 import UCF101
 from datasets.hmdb51 import HMDB51
+from datasets.ucfcrime import UCFCRIME
+
+
+DATASET_CLASS = {
+    'kinetics': Kinetics,
+    'activitynet':
+        lambda untrimmed: lambda *args, **kwargs: ActivityNet(is_untrimmed_setting=untrimmed, *args, **kwargs),
+    'ucf101': UCF101,
+    'UCF_CRIME': UCFCRIME,
+    'hmdb51': HMDB51,
+}
 
 
 def get_training_set(opt, spatial_transform, temporal_transform,
                      target_transform):
-    assert opt.dataset in ['kinetics', 'activitynet', 'ucf101', 'hmdb51']
+    assert opt.dataset in ['kinetics', 'activitynet', 'ucf101', 'hmdb51', 'UCF_CRIME']
 
-    if opt.dataset == 'kinetics':
-        training_data = Kinetics(
+    dataset = DATASET_CLASS[opt.dataset]
+
+    if opt.dataset == 'activitynet':
+        dataset = dataset(False)
+
+    training_data = dataset(
             opt.video_path,
             opt.annotation_path,
             'training',
             spatial_transform=spatial_transform,
             temporal_transform=temporal_transform,
-            target_transform=target_transform)
-    elif opt.dataset == 'activitynet':
-        training_data = ActivityNet(
-            opt.video_path,
-            opt.annotation_path,
-            'training',
-            False,
-            spatial_transform=spatial_transform,
-            temporal_transform=temporal_transform,
-            target_transform=target_transform)
-    elif opt.dataset == 'ucf101':
-        training_data = UCF101(
-            opt.video_path,
-            opt.annotation_path,
-            'training',
-            spatial_transform=spatial_transform,
-            temporal_transform=temporal_transform,
-            target_transform=target_transform)
-    elif opt.dataset == 'hmdb51':
-        training_data = HMDB51(
-            opt.video_path,
-            opt.annotation_path,
-            'training',
-            spatial_transform=spatial_transform,
-            temporal_transform=temporal_transform,
-            target_transform=target_transform)
+            target_transform=target_transform
+    )
 
     return training_data
 
 
 def get_validation_set(opt, spatial_transform, temporal_transform,
                        target_transform):
-    assert opt.dataset in ['kinetics', 'activitynet', 'ucf101', 'hmdb51']
+    assert opt.dataset in ['kinetics', 'activitynet', 'ucf101', 'hmdb51', 'UCF_CRIME']
 
-    if opt.dataset == 'kinetics':
-        validation_data = Kinetics(
-            opt.video_path,
-            opt.annotation_path,
-            'validation',
-            opt.n_val_samples,
-            spatial_transform,
-            temporal_transform,
-            target_transform,
-            sample_duration=opt.sample_duration)
-    elif opt.dataset == 'activitynet':
-        validation_data = ActivityNet(
-            opt.video_path,
-            opt.annotation_path,
-            'validation',
-            False,
-            opt.n_val_samples,
-            spatial_transform,
-            temporal_transform,
-            target_transform,
-            sample_duration=opt.sample_duration)
-    elif opt.dataset == 'ucf101':
-        validation_data = UCF101(
-            opt.video_path,
-            opt.annotation_path,
-            'validation',
-            opt.n_val_samples,
-            spatial_transform,
-            temporal_transform,
-            target_transform,
-            sample_duration=opt.sample_duration)
-    elif opt.dataset == 'hmdb51':
-        validation_data = HMDB51(
-            opt.video_path,
-            opt.annotation_path,
-            'validation',
-            opt.n_val_samples,
-            spatial_transform,
-            temporal_transform,
-            target_transform,
-            sample_duration=opt.sample_duration)
+    dataset = DATASET_CLASS[opt.dataset]
+
+    if opt.dataset == 'activitynet':
+        dataset = dataset(False)
+
+    validation_data = dataset(
+        opt.video_path,
+        opt.annotation_path,
+        'validation',
+        n_samples_for_each_video=opt.n_val_samples,
+        spatial_transform=spatial_transform,
+        temporal_transform=temporal_transform,
+        target_transform=target_transform,
+        sample_duration=opt.sample_duration
+    )
+
     return validation_data
 
 
 def get_test_set(opt, spatial_transform, temporal_transform, target_transform):
-    assert opt.dataset in ['kinetics', 'activitynet', 'ucf101', 'hmdb51']
+    assert opt.dataset in ['kinetics', 'activitynet', 'ucf101', 'hmdb51', 'UCF_CRIME']
     assert opt.test_subset in ['val', 'test']
 
     if opt.test_subset == 'val':
         subset = 'validation'
     elif opt.test_subset == 'test':
         subset = 'testing'
-    if opt.dataset == 'kinetics':
-        test_data = Kinetics(
-            opt.video_path,
-            opt.annotation_path,
-            subset,
-            0,
-            spatial_transform,
-            temporal_transform,
-            target_transform,
-            sample_duration=opt.sample_duration)
-    elif opt.dataset == 'activitynet':
-        test_data = ActivityNet(
-            opt.video_path,
-            opt.annotation_path,
-            subset,
-            True,
-            0,
-            spatial_transform,
-            temporal_transform,
-            target_transform,
-            sample_duration=opt.sample_duration)
-    elif opt.dataset == 'ucf101':
-        test_data = UCF101(
-            opt.video_path,
-            opt.annotation_path,
-            subset,
-            0,
-            spatial_transform,
-            temporal_transform,
-            target_transform,
-            sample_duration=opt.sample_duration)
-    elif opt.dataset == 'hmdb51':
-        test_data = HMDB51(
-            opt.video_path,
-            opt.annotation_path,
-            subset,
-            0,
-            spatial_transform,
-            temporal_transform,
-            target_transform,
-            sample_duration=opt.sample_duration)
+
+    dataset = DATASET_CLASS[opt.dataset]
+
+    if opt.dataset == 'activitynet':
+        dataset = dataset(True)
+
+    test_data = dataset(
+        opt.video_path,
+        opt.annotation_path,
+        subset,
+        n_samples_for_each_video=0,
+        spatial_transform=spatial_transform,
+        temporal_transform=temporal_transform,
+        target_transform=target_transform,
+        sample_duration=opt.sample_duration
+    )
 
     return test_data
---
+++ datasets/ucfcrime.py
@@ -0,0 +1,23 @@
+import re
+from datasets.ucf101 import *
+
+
+class UCFCRIME(UCF101):
+    @classmethod
+    def get_video_names_and_annotations(cls, data, subset):
+        video_names = []
+        annotations = []
+
+        for key, value in data['database'].items():
+            this_subset = value['subset']
+            if this_subset == subset:
+                folder = re.match(r'(\w*)\d{3}_x264', key).group(1)
+
+                # if folder == 'Normal_Videos_':
+                #     folder = '../Normal_Videos_for_Event_Recognition'
+
+                video_names.append('{}/{}'.format(folder, key))
+                annotations.append(value['annotations'])
+
+        return video_names, annotations
+
